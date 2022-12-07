9b2357cf29c49c8220cebdfb4df5b063e4bfe608
abk237 <abk@gmail.com>
2022-12-07 09:58:58 +0100

    add create account and login function

---
+++ lib/Screens/HomeScreen.dart
@@ -0,0 +1,24 @@
+import 'package:fax/Authenticate/Methods.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Screen"),
+        actions: [
+          IconButton(icon: Icon(Icons.logout), onPressed: () => logOut(context))
+        ],
+      ),
+    );
+  }
+}
